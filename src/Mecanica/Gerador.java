46e4c6e0f2bbbdc2f57c590672e539daefc75789
Cerry <denis.carreto@hotmail.com>
2025-07-23 12:45:55 -0300

    Initial commit

---
+++ src/Mecanica/Gerador.java
@@ -0,0 +1,36 @@
+package Mecanica;
+
+import java.util.Random;
+
+public class Gerador {
+
+    public String gerarSenhaUnica(int digitos, String parametros) {
+        String letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
+        String letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numeros = "0123456789";
+        String especiais = "+=!@#$%&*()-_[]{}|;:\"<>,./?\\";
+        StringBuilder caracteres = new StringBuilder(letrasMinusculas); // Sempre minúsculas
+
+        if (parametros.contains("V")) caracteres.append(letrasMaiusculas);
+        if (parametros.contains("F")) caracteres.append(numeros);
+        if (parametros.contains("E")) caracteres.append(especiais);
+
+        StringBuilder senha = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < digitos; i++) {
+            int index = random.nextInt(caracteres.length());
+            senha.append(caracteres.charAt(index));
+        }
+
+        return senha.toString();
+    }
+
+    public String[] gerarSenhasArray(int quantidade, int tamanho, String parametros) {
+        String[] lista = new String[quantidade];
+        for (int i = 0; i < quantidade; i++) {
+            lista[i] = gerarSenhaUnica(tamanho, parametros);
+        }
+        return lista;
+    }
+}
---
+++ src/Mecanica/InterfaceGrafica.java
@@ -0,0 +1,66 @@
+package Mecanica;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class InterfaceGrafica extends Application {
+
+    Gerador gerador = new Gerador();
+
+    @Override
+    public void start(Stage stage) {
+        Label labelQuantidade = new Label("Quantidade de senhas:");
+        TextField campoQuantidade = new TextField("5");
+
+        Label labelTamanho = new Label("Tamanho das senhas:");
+        TextField campoTamanho = new TextField("10");
+
+        CheckBox maiusculas = new CheckBox("Incluir letras maiúsculas");
+        CheckBox numeros = new CheckBox("Incluir números");
+        CheckBox especiais = new CheckBox("Incluir caracteres especiais");
+
+        Button botao = new Button("Gerar Senhas");
+        TextArea output = new TextArea();
+        output.setEditable(false);
+        output.setWrapText(true);
+
+        botao.setOnAction(e -> {
+            try {
+                int tamanho = Integer.parseInt(campoTamanho.getText());
+                int quantidade = Integer.parseInt(campoQuantidade.getText());
+
+                StringBuilder parametros = new StringBuilder();
+                if (maiusculas.isSelected()) parametros.append("V");
+                if (numeros.isSelected()) parametros.append("F");
+                if (especiais.isSelected()) parametros.append("E");
+
+                String[] senhas = gerador.gerarSenhasArray(quantidade, tamanho, parametros.toString());
+
+                output.setText("");
+                for (String senha : senhas) {
+                    output.appendText(senha + "\n");
+                }
+
+            } catch (NumberFormatException ex) {
+                output.setText("Por favor, insira valores numéricos válidos.");
+            }
+        });
+
+        VBox root = new VBox(10, labelQuantidade, campoQuantidade, labelTamanho, campoTamanho,
+                maiusculas, numeros, especiais, botao, output);
+        root.setPadding(new Insets(15));
+
+        Scene scene = new Scene(root, 400, 450);
+        stage.setTitle("Gerador de Senhas");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
